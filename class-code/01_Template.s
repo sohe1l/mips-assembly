812427d918065d93305033be11a0ae2fdc5b8a9b
sohe1l <soheilansari@icloud.com>
2019-09-13 09:24:01 -0700

    Add started template for MAL.

---
+++ class-code/01_Template.s
@@ -0,0 +1,21 @@
+# Comment giving name of program and description of function
+# Template.s
+# Bare-bones outline of MIPS assembly language program
+
+# Constants Definition
+.eqv SYS_PRINT_STRING 4
+
+# Data Section of the Program
+
+           .data       # variable declarations follow this line
+                       # ...
+		
+# Text Section of the Program
+																						
+           .text       # instructions follow this line	
+																	
+main:                  # indicates start of code (first instruction to execute)
+                       # ...
+                       
+									
+# End of program, leave a blank line afterwards to make SPIM happy
