c82e0062e73d43b4a987ad2984b6e0098d0aea6f
sohe1l <soheilansari@icloud.com>
2019-09-27 15:46:22 -0700

    Update 05_Input.s

--- class-code/05_Input.s
+++ class-code/05_Input.s
@@ -1,12 +1,22 @@
 # Examples for getting input from the console
 # First we are defining system calls for better readability
+.eqv PRINT_INT 1
+.eqv INPUT_INT 5
 .eqv SYS_EXIT 10
 
 	.data
 	
 	.text																
 main:
-		# TODO
+		# Read into and stores in $v0
+		li $v0, INPUT_INT
+		syscall			
+		move $s0, $v0
+		
+		# Print $s0 into the console.
+		li $v0, PRINT_INT
+		move $a0, $s0
+		syscall
 		
 		li $v0, SYS_EXIT
 		syscall			# program exits.
